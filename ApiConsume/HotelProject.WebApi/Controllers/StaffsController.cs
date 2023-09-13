c8f5b5463565491e6f2214c95e3463ab874a9af9
bsergenak <berkaysergenak@hotmail.com>
2023-09-14 02:53:21 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/StaffsController.cs
@@ -0,0 +1,51 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffsController : ControllerBase
+    {
+        private readonly IStaffService _staffService;
+
+        public StaffsController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+
+        [HttpGet]
+        public IActionResult StaffList()
+        {
+            var values = _staffService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddStaff(Staff staff)
+        {
+            _staffService.TInsert(staff);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteStaff(int id)
+        {
+            var values = _staffService.TGetById(id);
+            _staffService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateStaff(Staff staff)
+        {
+            _staffService.TUpdate(staff);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStaff(int id)
+        {
+            var values = _staffService.TGetById(id);
+            return Ok(values);
+        }
+    }
+}
