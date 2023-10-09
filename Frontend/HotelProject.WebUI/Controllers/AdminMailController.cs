de46bc899568a2a3ffa9b241cc024f4e4fb03d74
bsergenak <berkaysergenak@hotmail.com>
2023-10-09 15:56:38 +0300

    mail işlemleri yapıldı, image işlemleri yapılıyor

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,41 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel model)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddress = new MailboxAddress("HotelierAdmin", "berkaysergen.ak@gmail.com");
+            mimeMessage.From.Add(mailboxAddress);
+
+            MailboxAddress mailboxAdressTo = new MailboxAddress("User", model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAdressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = model.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smpt.gmail.com", 587, false);
+            client.Authenticate("berkaysergen.ak@gmail.com", "syhl xwsp dlln ioja");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            return View();
+        }
+    }
+}
