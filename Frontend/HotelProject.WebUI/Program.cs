af118bf7bb1b909b371190ff07a429db61aacbba
bsergenak <berkaysergenak@hotmail.com>
2023-10-10 14:35:48 +0300

    Api dosya islemleri ve authorize, allowanonymous islemleri

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -4,6 +4,8 @@ using HotelProject.DataAccessLayer.Concrete;
 using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.Dtos.GuestDto;
 using HotelProject.WebUI.ValidationRules.GuestValidationRules;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,20 @@ builder.Services.AddTransient<IValidator<CreateGuestDto>, GuestCreateValidator>(
 builder.Services.AddTransient<IValidator<UpdateGuestDto>, GuestUpdateValidator>();
 builder.Services.AddControllersWithViews().AddFluentValidation();
 builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddMvc(config =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+    .RequireAuthenticatedUser()
+    .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+    options.LoginPath = "/Login/Index/";
+});
 
 var app = builder.Build();
 
@@ -24,7 +40,13 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404", "?code={0}");
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+
+app.UseAuthentication();
 
 app.UseRouting();
 
